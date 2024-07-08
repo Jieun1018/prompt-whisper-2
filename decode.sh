1e9c4f532baaaaf6c6cfb46bd547d687de1f57c3
Jieun1018 <1018uni@gmail.com>
2024-07-09 02:09:14 +0900

    code

--- decode.sh
+++ decode.sh
@@ -1,4 +1,4 @@
-#torchrun --nproc_per_node 1 ./clairaudience/main_multi.py ./config/cfg_sitec_ft_1.json
+torchrun --nproc_per_node 1 ./clairaudience/main_multi.py ./config/cfg_sitec_ft_1.json
 torchrun --nproc_per_node 1 ./clairaudience/main_multi.py ./config/cfg_sitec_ft_2.json
 torchrun --nproc_per_node 1 ./clairaudience/main_multi.py ./config/cfg_sitec_ft_3.json
 torchrun --nproc_per_node 1 ./clairaudience/main_multi.py ./config/cfg_sitec_ft_fake_1.json
