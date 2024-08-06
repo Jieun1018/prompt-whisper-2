47a54fc1432b0ce77adf7a47f4ba7112abfcc8f1
Jieun1018 <1018uni@gmail.com>
2024-08-07 01:40:58 +0900

    shell script

---
+++ run_largev3_fake_new.sh
@@ -0,0 +1,5 @@
+torchrun --nproc_per_node 8 ./clairaudience/main_multi.py ./config/large_v3_fake_new/cfg_sitec_ft_largev3_fake_new_1.json
+torchrun --nproc_per_node 8 ./clairaudience/main_multi.py ./config/large_v3_fake_new/cfg_sitec_ft_largev3_fake_new_2.json
+torchrun --nproc_per_node 8 ./clairaudience/main_multi.py ./config/large_v3_fake_new/cfg_sitec_ft_largev3_fake_new_3.json
+torchrun --nproc_per_node 8 ./clairaudience/main_multi.py ./config/large_v3_fake_new/cfg_sitec_ft_largev3_fake_new_4.json
+torchrun --nproc_per_node 8 ./clairaudience/main_multi.py ./config/large_v3_fake_new/cfg_sitec_ft_largev3_fake_new_5.json
