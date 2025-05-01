8123e1171710bf0154b2774c1fbcc2f2f6a66f68
Jieun <1018uni@gmail.com>
2025-05-01 15:14:23 +0900

    libri-noisy code: running script

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-torchrun --nproc_per_node 2 ./clairaudience/main_multi.py ./config/cfg_sitec_ft_natural_distance.json
+torchrun --nproc_per_node 2 ./clairaudience/main_multi.py ./config/cfg_libri-noisy_ft_snr.json
 #torchrun --nproc_per_node 8 ./clairaudience/main_multi.py ./config/cfg_sitec_ft_largev3_1_2000.json
 #torchrun --nproc_per_node 8 ./clairaudience/main_multi.py ./config/cfg_sitec_ft_largev3_2_2000.json
 #torchrun --nproc_per_node 8 ./clairaudience/main_multi.py ./config/cfg_sitec_ft_largev3_3_2000.json
